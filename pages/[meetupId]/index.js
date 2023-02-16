306528284d741ccd4642dfec160a72c41af90d2f
Sagar Mittal <mittalsagar006@gmail.com>
2023-02-16 14:01:40 +0530

    adding component for meetupDetal

---
+++ pages/[meetupId]/index.js
@@ -0,0 +1,14 @@
+import MeetupDetail from '../../components/meetups/MeetupDetail';
+
+const MeetupDetails = () => {
+  return (
+    <MeetupDetail
+      title="First Meetup"
+      image="https://images.pexels.com/photos/1709003/pexels-photo-1709003.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+      address="Billford Stret, Minnesota"
+      description="This is the first meetup"
+    />
+  );
+};
+
+export default MeetupDetails;
